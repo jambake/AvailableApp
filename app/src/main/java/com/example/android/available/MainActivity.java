852176adec0f0a128ce5cdc4c229c4c78913a77f
Jim Baker <jim.baker1217@gmail.com>
2017-01-19 15:23:59 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/android/available/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.android.available;
+
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    protected static final String TAG = MainActivity.class.getSimpleName();
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+    protected LocationRequest mLocationRequest;
+    protected Location mCurrentLocation;
+    protected ImageButton getLocationButton;
+
+    protected Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+    protected List<android.location.Address> addresses;
+
+    protected TextView mStreetText;
+    protected TextView mCityText;
+    protected TextView mStateText;
+    protected TextView mZipCodeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLatitudeText = (TextView) findViewById(R.id.lat_val);
+        mLongitudeText = (TextView) findViewById(R.id.long_val);
+        mStreetText = (TextView) findViewById(R.id.street_number_name);
+        mCityText = (TextView) findViewById(R.id.city_name);
+        mStateText = (TextView) findViewById(R.id.state_name);
+        mZipCodeText = (TextView) findViewById(R.id.zip_code);
+
+        buildGoogleApiClient();
+
+        getLocationButton = (ImageButton) findViewById(R.id.get_location);
+        getLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {addresses = geocoder.getFromLocation(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(), 3);}
+                catch (IOException e) {e.printStackTrace();}
+
+                String address = addresses.get(0).getAddressLine(0);
+                String city = addresses.get(0).getLocality();
+                String state = addresses.get(0).getAdminArea();
+                String postalCode = addresses.get(0).getPostalCode();
+
+                mStreetText.setText(address);
+                mCityText.setText(city);
+                mStateText.setText(state);
+                mZipCodeText.setText(postalCode);
+
+                mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
+                mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
+
+
+            }
+        });
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        try { LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this); }
+        catch (SecurityException securityException) { securityException.printStackTrace(); }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, location.toString());
+
+        mCurrentLocation = location;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    public void onDisconnected() {
+        Log.i(TAG, "Disconnected");
+    }
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
